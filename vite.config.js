775d575c9c1f90e1eea193fa8b7a8d29ccd9f5b4
Darwin <Wazapwzap@gmail.com>
2026-03-01 11:29:02 +0800

    final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio2/",
+  
   plugins: [
     react({
       babel: {
@@ -11,4 +11,5 @@ export default defineConfig({
       },
     }),
   ],
+  base: "/portfolio2/",
 })
